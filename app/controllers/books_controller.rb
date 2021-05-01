6ca13012e3486ba74076db200885a03263cb63b1
korethisyo <korethis0816@gmail.com>
2021-05-01 04:59:23 +0000

    [Update]work1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,6 +28,8 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
+    binding.pry
+
     if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
